4567b312f1b86a6eded06b4b1c80bc282b26c776
capyyimg <79929439+capyyimg@users.noreply.github.com>
2021-03-04 13:36:46 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,6 +13,7 @@ input.onGesture(Gesture.SixG, function () {
     subtrace = 0
     sum = 0
 })
+// When you are pressing A and B button it does number 2-number 1 and i set up the code so that it shows the number of value 2 and then the minus sign and then shows the value of number 1 and the last thing is that it shows equal sign and then it shows the sum.
 input.onButtonPressed(Button.AB, function () {
     sum = number_2 + number_1
     basic.showNumber(number_2)
@@ -69,9 +70,11 @@ input.onGesture(Gesture.TiltRight, function () {
     number_1 += -1
     basic.showNumber(number_1)
 })
+// When the application starts, you will want to set all the variables to 0.
 let sum = 0
 let subtrace = 0
 let number_1 = 0
 let number_2 = 0
 number_2 = 0
 number_1 = 0
+number_2 = 0
